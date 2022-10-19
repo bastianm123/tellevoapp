5861b6b72cc1990db3f8df5bc381653e920b5ae2
bastianm123 <bastian-gallet@hotmail.com>
2022-10-19 18:31:24 -0300

    Primera entrega

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,16 +8,47 @@ import { ServicesService } from '../services/services.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  field: string = "";
+  viaje = {
+    origen:"",
+    destino:"",
+    precio:"",
+  }
   nombre = localStorage.getItem("nombre");
   user = {
     usuario: "",
     password: ""
   }
   constructor(
+    public toastController: ToastController,
     public alertController: AlertController,
     private loadingCtrl: LoadingController,
     private api: ServicesService
   ){}
+  enviarViaje(){
+    if(this.validateModel(this.viaje)){
+      this.presentToast1()
+    }
+    else {
+      this.presentToast("Falta ingresar: " + this.field, 4500);
+    }
+  }
+  async presentToast(msg: string, duracion?: number) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: duracion ? duracion : 2000
+    });
+    toast.present()
+  }
+  validateModel(model: any) {
+    for (var [key, value] of Object.entries(model)) {
+      if (value == "") {
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
   async presentToast1(){
     const alert = await this.alertController.create({
       message: "Tu viaje a sido publicado",
